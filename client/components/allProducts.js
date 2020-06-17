88f5b1af29ba75eff3152dcb5ebe642e4e5fa7fe
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-17 16:39:44 -0400

    create fake data

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -2,6 +2,24 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchProducts, deleteProduct} from '../store/allProducts'
 
+const fakeExp = [
+  {
+    name: 'NYC Landmark Tours',
+    description: 'Take a tour of NYC landmarks',
+    price: 125,
+    imageUrl:
+      'https://www.xperiencedays.com/images/Philadelphia-Introductory-Flight-Lessons-XD-1665-002_700x410.jpg',
+    category: 'activity'
+  },
+  {
+    name: 'PHILADELPHIA DISCOVERY FLIGHT LESSON',
+    description: 'Exciting travel plans',
+    price: 300,
+    imageUrl:
+      'https://www.xperiencedays.com/images/Philadelphia-Introductory-Flight-Lessons-XD-1665-002_700x410.jpg',
+    category: 'activity'
+  }
+]
 class AllProducts extends React.Component {
   constructor(props) {
     super(props)
