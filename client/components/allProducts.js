b5339c20b9f0fd273454f024f07e6b66012dd3c7
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-17 14:12:00 -0400

    impement AllProducts class component

--- client/components/allProducts.js
+++ client/components/allProducts.js
@@ -1,2 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {fetchProducts, deleteProduct} from '../store/allProducts'
 
+class AllProducts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+  render() {
+    return <h1>PRODUCTS</h1>
+  }
+}
+
+const mapStateToProps = state => {
+  return {products: state.products}
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: () => dispatch(fetchProducts()),
+  deleteProduct: id => dispatch(deleteProduct(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
