9e823eba26b29b7089fa552aa9036ac5dcc845e1
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-17 12:38:50 -0400

    implementation of allProducts reducer

---
+++ client/store/allProducts.js
@@ -0,0 +1,56 @@
+import axios from 'axios'
+import history from '../history'
+
+//action types
+const GET_PRODUCTS = 'GET_PRODUCTS'
+const DELETE_PRODUCT = 'DELETE_PRODUCT'
+
+//action creators
+export const getProducts = requestedProducts => ({
+  type: GET_PRODUCTS,
+  requestedProducts
+})
+
+export const removeProduct = requestedProduct => ({
+  type: DELETE_PRODUCT,
+  requestedProduct
+})
+
+//state
+const initalState = []
+
+//thunk
+export const fetchProducts = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get(`/api/products`)
+      dispatch(getProducts(data || initalState))
+      history.push('/allExperiences')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const deleteProduct = id => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.delete(`/api/products/${id}`)
+      dispatch(removeProduct(data || initalState))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+//reducer
+export default (state = initalState, action) => {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {...state, products: action.requestedProducts}
+    case DELETE_PRODUCT:
+      return {...state}
+    default:
+      return state
+  }
+}
---
+++ client/store/singleProduct.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import history from '../history'
+
+//action types
+const SET_PRODUCT = 'GET_PRODUCT'
+const EDIT_PRODUCT = 'EDIT_PRODUCT'
+
+//action creators
+
+//thunk
+
+//state
+
+//reducer
