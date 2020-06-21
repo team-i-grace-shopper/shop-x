e6b42ec6b90f6b2c3d1317d96afae791862fd508
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-21 17:32:19 -0400

    route & path to /shop

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,6 +15,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/shop">Shop</Link>
           <Link to="/cart">Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
@@ -25,6 +26,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
+          <Link to="/shop">Shop</Link>
           <Link to="/cart">Cart</Link>
         </div>
       )}
