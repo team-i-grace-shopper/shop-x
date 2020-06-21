52b896aa9c155d9e6b963a9cca8de4c394206012
Treagan Birbal <treaganbirbal@pursuit.org>
2020-06-21 13:33:51 -0400

    cart route links to /cart

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -25,7 +25,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           {}
-          <Link to="#">Cart</Link>
+          <Link to="/cart">Cart</Link>
         </div>
       )}
     </nav>
