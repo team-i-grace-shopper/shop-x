c721e8779f973d160d690019c1380fd8bd7a5dd0
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-06-22 15:45:15 -0400

    convert Cart component to class component, add flex box for totals

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import CartItem from './CartItem'
-let items = JSON.parse(localStorage.getItem('cart'))
 
-export default function Cart(props) {
-  if (!items) {
-    return <h1>There are no items in the cart</h1>
+let items = JSON.parse(localStorage.getItem('cart'))
+let totalItems = items.reduce(function(accum, currV) {
+  return accum + currV.quantity
+}, 0)
+
+export default class Cart extends React.component {
+  constructor() {
+    super(props)
   }
 
-  return (
-    <div className="cartAll">
-      {items.map(item => {
-        return <CartItem item={item} />
-      })}
-    </div>
-  )
+  render() {
+    if (!items) {
+      return <h1>There are no items in the cart</h1>
+    }
+
+    return (
+      <div className="cart">
+        <div className="cartAll">
+          {items.map(item => {
+            return <CartItem item={item} />
+          })}
+        </div>
+        <div>
+          <h1>Totals:</h1>
+          <h2>Total items in cart: {totalItems}</h2>
+        </div>
+      </div>
+    )
+  }
 }
