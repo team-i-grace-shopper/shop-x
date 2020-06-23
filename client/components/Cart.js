2bf6e10ee1e61e2b8746c1fe4fe8d6f8cfe9e8a1
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-06-23 13:03:07 -0400

    saving work, quantity does not work yet

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import CartItem from './CartItem'
 
-let items = JSON.parse(localStorage.getItem('cart'))
-let totalItems
+let items = null
+if (localStorage.getItem('cart')) {
+  items = JSON.parse(localStorage.getItem('cart'))
+}
+console.log(localStorage.getItem('cart'))
+let totalItems, totalDue
 
 class Cart extends React.Component {
   constructor(props) {
     super(props)
   }
 
-  componentDidMount() {
-    totalItems = items.reduce(function(accum, currV) {
-      return accum + currV.quantity
-    }, 0)
-  }
+  // componentDidMount() {
+  //   totalItems = items.reduce(function (accum, currV) {
+  //     return accum + currV.quantity
+  //   }, 0)
+  // }
 
   render() {
+    // console.log(items)
     if (!items) {
       return <h1>There are no items in the cart</h1>
     }
 
     totalItems = items.reduce(function(accum, currV) {
       return accum + currV.quantity
+    }, 0)
+
+    totalDue = items.reduce(function(accum, currV) {
+      return accum + currV.price * currV.quantity
     }, 0)
 
     return (
@@ -34,6 +43,7 @@ class Cart extends React.Component {
         <div>
           <h1>Totals:</h1>
           <h2>Total items in cart: {totalItems}</h2>
+          <h2>Total due: {totalDue} $</h2>
         </div>
       </div>
     )
