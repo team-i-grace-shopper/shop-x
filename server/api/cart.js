da7b865783afb4700842eb2653ac846949ee21e5
Ekaterina Serga <56270336+EkaterinaSerga@users.noreply.github.com>
2020-06-24 09:45:47 -0400

    Update cart.js

--- server/api/cart.js
+++ server/api/cart.js
@@ -13,11 +13,6 @@ router.post('/', async (req, res, next) => {
     const order = await Order.create({userId})
 
     req.body.products.forEach(async product => {
-      //   const expItem = await Product.findOne({
-      //     where: {
-      //       id: product.id
-      //     }
-      // })
 
       await OrderDetail.create({
         orderId: Number(order.id),
@@ -27,44 +22,13 @@ router.post('/', async (req, res, next) => {
       })
     })
 
-    // await order.addProduct(product, {through: OrderDetail})
-
     console.log('product added')
 
-    // req.body.products.map((product) => {
-    //   return order.addProduct(
-    //     // product.product,
-    //     {through: OrderDetail},
-    //     // {productQty: Number(product.quantity)}
-    //   )
-    // })
     res.status(200).send('You order has been created')
   } catch (error) {
     console.log(error)
   }
 })
-
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const products = await Product.findAll()
-//     res.json(products)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
-// router.get('/:productId', async (req, res, next) => {
-//   try {
-//     const product = await Product.findByPk(req.params.productId)
-//     if (!product) {
-//       res.status(404).send('not found')
-//     } else {
-//       res.status(200).json(product)
-//     }
-//   } catch (err) {
-//     next(err)
-//   }
-// })
 
 router.put('/:id', async (req, res, next) => {
   try {
